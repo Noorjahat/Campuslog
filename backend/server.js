e8614a45c3e03be0c21e03295c46eda5e0143e54
Noorjahat <galvestongamer21@gmail.com>
2026-03-16 11:24:22 +0530

    upd

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,11 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
 const path = require("path");
 
 // 👉 frontend folder serve karega
@@ -10,14 +18,6 @@ app.get("/", (req, res) => {
 app.get("/admin", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/admin.html"));
 });
-
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-
-const app = express();
-app.use(cors());
-app.use(express.json());
 
 // 🔹 MongoDB connect
 mongoose.connect(process.env.MONGO_URI);
