b4cbc3e43d65f4670f35d62e26543db32504dfed
Noorjahat <galvestongamer21@gmail.com>
2026-03-16 08:30:10 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 // 🔹 MongoDB connect
-mongoose.connect("mongodb://127.0.0.1:27017/campuslog");
+mongoose.connect(process.env.MONGO_URI);
 
 // 🔹 PC Schema
 const pcSchema = new mongoose.Schema({
